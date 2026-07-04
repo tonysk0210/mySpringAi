d6dab888877f79a7ebb81a4452eef4b8ccad4b02
Tony Shangkuan @ Desktop <tonyshangkuan@gmail.com>
2026-07-04 15:56:22 +0800

    prometheus and grafana

--- src/main/java/com/example/mySpringAi/config/VectorStoreConfig.java
+++ src/main/java/com/example/mySpringAi/config/VectorStoreConfig.java
@@ -1,5 +1,6 @@
 package com.example.mySpringAi.config;
 
+import io.micrometer.observation.ObservationRegistry;
 import io.qdrant.client.QdrantClient;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.embedding.TokenCountBatchingStrategy;
@@ -19,20 +20,22 @@ public class VectorStoreConfig {
      * EmbeddingModel embeddingModel：用來把文字轉成向量；你專案裡目前 EmbeddingModelConfig 用 @Primary 指定 OpenAI embedding 為主要模型
      */
     @Bean("pdfVectorStore")
-    public VectorStore pdfVectorStore(QdrantClient qdrantClient, EmbeddingModel embeddingModel) {
+    public VectorStore pdfVectorStore(QdrantClient qdrantClient, EmbeddingModel embeddingModel, ObservationRegistry observationRegistry) {
 
         return QdrantVectorStore.builder(qdrantClient, embeddingModel).collectionName("pdf-collection") // 這個 VectorStore 專門存取 Qdrant 裡的 pdf-collection
                 .initializeSchema(true) // 啟動時如果 collection/schema 不存在，會嘗試建立
                 .batchingStrategy(new TokenCountBatchingStrategy())  // 使用 token 數量分批送 embedding；這也是 Spring AI 的預設策略
+                .observationRegistry(observationRegistry) // 注入 ObservationRegistry，讓 Micrometer 可以追蹤 db.vector.client.operation 指標
                 .build();
     }
 
     @Bean("cachingVectorStore")
-    public VectorStore cachingVectorStore(QdrantClient qdrantClient, EmbeddingModel embeddingModel) {
+    public VectorStore cachingVectorStore(QdrantClient qdrantClient, EmbeddingModel embeddingModel, ObservationRegistry observationRegistry) {
 
         return QdrantVectorStore.builder(qdrantClient, embeddingModel).collectionName("caching-collection") // 這個 VectorStore 專門存取 Qdrant 裡的 pdf-collection
                 .initializeSchema(true) // 啟動時如果 collection/schema 不存在，會嘗試建立
                 .batchingStrategy(new TokenCountBatchingStrategy())  // 使用 token 數量分批送 embedding；這也是 Spring AI 的預設策略
+                .observationRegistry(observationRegistry) // 注入 ObservationRegistry，讓 Micrometer 可以追蹤 db.vector.client.operation 指標
                 .build();
     }
 }
