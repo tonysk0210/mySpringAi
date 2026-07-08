3cf2dfda850a7dd7d6400dfbc83b0802a8544013
Tony Shangkuan @ Desktop <tonyshangkuan@gmail.com>
2026-07-08 22:54:10 +0800

    update

--- src/main/java/com/example/mySpringAi/advisor/PrettyLoggerAdvisor.java
+++ src/main/java/com/example/mySpringAi/advisor/PrettyLoggerAdvisor.java
@@ -1,4 +1,4 @@
-package com.example.myagentclient.advisor;
+package com.example.mySpringAi.advisor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClientRequest;
